61a0d1fe230e8c441c475d6b5648e96813195816
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-17 03:32:42 -0300

    Criação dos atributos das entidades da pasta Models

--- Apetit_API_2019/Apetit_API_2019/Models/Cli_Nutricionista.cs
+++ Apetit_API_2019/Apetit_API_2019/Models/Cli_Nutricionista.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,62 @@ namespace Apetit_API_2019.Models
 {
     public class Cli_Nutricionista
     {
+      
+            public int IdNutricionista { get; set; }
+            [Required]
+            public String Nome { get; set; }
+            [Required]
+            public DateTime Dtnascimento { get; set; }
+            [Required]
+            public String Sexo { get; set; }
+            [Required]
+            public String Cpf { get; set; }
+            [Required]
+            public String Email { get; set; }
+            [Required]
+            public String Telefone { get; set; }
+            [Required]
+            public String Endereco { get; set; }
+            [Required]
+            public String Cep { get; set; }
+            [Required]
+            public String Complemento { get; set; }
+            [Required]
+            public String Bairro { get; set; }
+            [Required]
+            public String Login { get; set; }
+            [Required]
+            public String Senha { get; set; }
+            [Required]
+            public String Dieta { get; set; }
+            [Required]
+            public String Cidade { get; set; }
+            [Required]
+            public DateTime Agconsulta { get; set; }
+            [Required]
+            public String CRN { get; set; }
+
+            public Cli_Nutricionista(int idnutricionista, string nome, DateTime dtnascimento, string sexo, string cpf, string email, string telefone, string endereco, string cep, string complemento, string bairro, string login, string senha, string dieta, string cidade, DateTime agconsulta, string crn)
+            {
+                this.IdNutricionista = idnutricionista;
+                this.Nome = nome;
+                this.Dtnascimento = dtnascimento;
+                this.Sexo = sexo;
+                this.Cpf = cpf;
+                this.Email = email;
+                this.Telefone = telefone;
+                this.Endereco = endereco;
+                this.Cep = cep;
+                this.Complemento = complemento;
+                this.Bairro = bairro;
+                this.Login = login;
+                this.Senha = senha;
+                this.CRN = crn;
+                this.Agconsulta = agconsulta;
+                
+                
+        }
+
+
     }
 }
--- Apetit_API_2019/Apetit_API_2019/Models/Cli_Paciente.cs
+++ Apetit_API_2019/Apetit_API_2019/Models/Cli_Paciente.cs
@@ -1,13 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Apetit_API_2019.Models
 {
-    public class Cli_Paciente
-    {
-        string nome { get; set; }
+    
+        public class Cli_Paciente
+        {
+            public int IdPaciente { get; set; }
+            [Required]
+            public String Nome { get; set; }
+            [Required]
+            public DateTime Dtnascimento { get; set; }
+            [Required]
+            public String Sexo { get; set; }
+            [Required]
+            public String Cpf { get; set; }
+            [Required]
+            public String Email { get; set; }
+            [Required]
+            public String Telefone { get; set; }
+            [Required]
+            public String Endereco { get; set; }
+            [Required]
+            public String Cep { get; set; }
+            [Required]
+            public String Complemento { get; set; }
+            [Required]
+            public String Bairro { get; set; }
+            [Required]
+            public String Login { get; set; }
+            [Required]
+            public String Senha { get; set; }
+            [Required]
+            public String Dieta { get; set; }
+            [Required]
+            public String Cidade { get; set; }
+            [Required]
+            public DateTime Agconsulta { get; set; }
+            [Required]
+            public double Peso { get; set; }
+            [Required]
+            public float Altura { get; set; }
 
-    }
+
+            public Cli_Paciente(int idpaciente, string nome, DateTime dtnascimento, string sexo, string cpf, string email, string telefone, string endereco, string cep, string complemento, string bairro, string login, string senha, string dieta, string cidade, DateTime agconsulta, float peso, float altura)
+            {
+                this.IdPaciente = idpaciente;
+                this.Nome = nome;
+                this.Dtnascimento = dtnascimento;
+                this.Sexo = sexo;
+                this.Cpf = cpf;
+                this.Email = email;
+                this.Telefone = telefone;
+                this.Endereco = endereco;
+                this.Cep = cep;
+                this.Complemento = complemento;
+                this.Bairro = bairro;
+                this.Login = login;
+                this.Senha = senha;
+                this.Dieta = dieta;
+                this.Agconsulta = agconsulta;
+                this.Peso = peso;
+                this.Altura = altura;
+            }
+        }
+
+
+    
 }
