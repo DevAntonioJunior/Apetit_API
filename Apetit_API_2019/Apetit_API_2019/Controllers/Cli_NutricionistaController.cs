472d8f6ebb29e12bb7366042120ed5b5011490ec
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-17 15:46:49 -0300

    Implementaçoes do Controller do Nutricionista

--- Apetit_API_2019/Apetit_API_2019/Controllers/Cli_NutricionistaController.cs
+++ Apetit_API_2019/Apetit_API_2019/Controllers/Cli_NutricionistaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Apetit_API_2019.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,76 @@ namespace Apetit_API_2019.Controllers
     [ApiController]
     public class Cli_NutricionistaController : ControllerBase
     {
+        private List<Cli_Nutricionista> listanutricionista = new List<Cli_Nutricionista>();
+
+
+        [AcceptVerbs("POST")]
+        [Route("CadastrarNutricionista")]
+        public string CadastrarNutricionista(Cli_Nutricionista nutricionista)
+        {
+            listanutricionista.Add(nutricionista);
+
+
+            return "Nutricionista adastrado com sucesso";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("AlterarNutricionista")]
+        public string AlterarNutricionista(Cli_Nutricionista nutricionista)
+        {
+            listanutricionista.Where(n => n.IdNutricionista == nutricionista.IdNutricionista)
+                .Select(s =>
+                {
+                    s.Login = nutricionista.Login;
+                    s.Senha = nutricionista.Senha;
+                    s.Nome = nutricionista.Nome;
+                    s.Dtnascimento = nutricionista.Dtnascimento;
+                    s.Endereco = nutricionista.Endereco;
+                    s.Email = nutricionista.Email;
+
+                    return s;
+
+                }).ToList();
+
+
+
+
+            return "Usuario alterado com sucesso";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluirrNutricionista")]
+        public string ExcluirUsuario(int idnutricionista)
+        {
+
+            Cli_Nutricionista nutricionista = listanutricionista.Where(n => n.IdNutricionista == idnutricionista)
+                .Select(n => n).FirstOrDefault();
+
+            listanutricionista.Remove(nutricionista);
+
+            return "Nutricionista excluido com sucesso";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("BuscarNutricionistaPorID/{idnutricionista}")]
+        public Cli_Nutricionista BuscarNutricionistaPorID(int idnutricionista)
+        {
+            Cli_Nutricionista nutricionista = listanutricionista.Where(n => n.IdNutricionista == idnutricionista)
+                .Select(n => n).FirstOrDefault();
+
+
+
+            return nutricionista; 
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("BuscarUsuarios")]
+        public List<Cli_Nutricionista> BuscarNutricionista()
+        {
+            return listanutricionista;
+        }
+
+
+
     }
 }
