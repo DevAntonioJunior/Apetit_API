23dd1e12ce41621e1d3d53035134549273bb8a1f
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-19 23:48:08 -0300

    Implementacao do Paciente Controller

--- Apetit_API_2019/Apetit_API_2019/Controllers/Cli_PacienteController.cs
+++ Apetit_API_2019/Apetit_API_2019/Controllers/Cli_PacienteController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Apetit_API_2019.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,69 @@ namespace Apetit_API_2019.Controllers
     [ApiController]
     public class Cli_PacienteController : ControllerBase
     {
+        private static List<Cli_Paciente> listapaciente = new List<Cli_Paciente>();
+
+
+        [AcceptVerbs("POST")]
+        [Route("CadastrarPaciente")]
+
+        public string CadastrarPaciente(Cli_Paciente paciente)
+        {
+            listapaciente.Add(paciente);
+
+
+            return "Paciente cadastrado com sucesso";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("EditarPaciente")]
+        public string EditarPaciente(Cli_Paciente paciente)
+        {
+
+            listapaciente.Where(n => n.IdPaciente == paciente.IdPaciente).Select(s =>
+            {
+                s.Login = paciente.Login;
+                s.Senha = paciente.Senha;
+                s.Nome = paciente.Nome;
+                s.Dtnascimento = paciente.Dtnascimento;
+                s.Endereco = paciente.Endereco;
+                s.Email = paciente.Email;
+
+                return s;
+            }).ToList();
+
+            return "Paciente alterado com sucesso!!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluiPaciente")]
+        public string ExcluirPaciente(int idpaciente)
+        {
+            Cli_Paciente paciente = listapaciente.Where(n => n.IdPaciente == idpaciente)
+                .Select(n => n).FirstOrDefault();
+            listapaciente.Remove(paciente);
+
+            return "Paciente excluido com sucesso";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("BucarPacientePorID/{idpaciente}")]
+        public Cli_Paciente BuscarPacienteporId(int idpaciente)
+        {
+            Cli_Paciente paciente = listapaciente.Where(n => n.IdPaciente == idpaciente)
+                .Select(n => n).FirstOrDefault();
+
+
+
+            return paciente;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("BuscarPaciente")]
+        public  List<Cli_Paciente> BuscarPaciente()
+        {
+            return listapaciente; 
+        }
+
     }
 }
