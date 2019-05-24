ea3c876750a81ecb21b25b88561b9c4bc0cfe2f9
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-24 02:36:21 -0300

    Remocao dos annotations e do construtor.

--- Apetit_API_2019/Apetit_API_2019/Models/Cli_Paciente.cs
+++ Apetit_API_2019/Apetit_API_2019/Models/Cli_Paciente.cs
@@ -6,67 +6,47 @@ using System.Threading.Tasks;
 
 namespace Apetit_API_2019.Models
 {
-    
-        public class Cli_Paciente
-        {
-            public int IdPaciente { get; set; }
-            [Key]
-            public String Nome { get; set; }
-            [Required]
-            public DateTime Dtnascimento { get; set; }
-            [Required]
-            public String Sexo { get; set; }
-            [Required]
-            public String Cpf { get; set; }
-            [Required]
-            public String Email { get; set; }
-            [Required]
-            public String Telefone { get; set; }
-            [Required]
-            public String Endereco { get; set; }
-            [Required]
-            public String Cep { get; set; }
-            [Required]
-            public String Complemento { get; set; }
-            [Required]
-            public String Bairro { get; set; }
-            [Required]
-            public String Login { get; set; }
-            [Required]
-            public String Senha { get; set; }
-            [Required]
-            public String Dieta { get; set; }
-            [Required]
-            public String Cidade { get; set; }
-            [Required]
-            public DateTime Agconsulta { get; set; }
-            [Required]
-            public double Peso { get; set; }
-            [Required]
-            public float Altura { get; set; }
+
+    public class Cli_Paciente
+    {
+        public int IdPaciente { get; set; }
+
+        public String Nome { get; set; }
+
+        public DateTime Dtnascimento { get; set; }
+
+        public String Sexo { get; set; }
+
+        public String Cpf { get; set; }
+
+        public String Email { get; set; }
+
+        public String Telefone { get; set; }
+
+        public String Endereco { get; set; }
+
+        public String Cep { get; set; }
+
+        public String Complemento { get; set; }
+
+        public String Bairro { get; set; }
+
+        public String Login { get; set; }
+
+        public String Senha { get; set; }
+
+        public String Dieta { get; set; }
+
+        public String Cidade { get; set; }
+
+        public DateTime Agconsulta { get; set; }
+
+        public double Peso { get; set; }
+
+        public float Altura { get; set; }
 
 
-            public Cli_Paciente(int idpaciente, string nome, DateTime dtnascimento, string sexo, string cpf, string email, string telefone, string endereco, string cep, string complemento, string bairro, string login, string senha, string dieta, string cidade, DateTime agconsulta, float peso, float altura)
-            {
-                this.IdPaciente = idpaciente;
-                this.Nome = nome;
-                this.Dtnascimento = dtnascimento;
-                this.Sexo = sexo;
-                this.Cpf = cpf;
-                this.Email = email;
-                this.Telefone = telefone;
-                this.Endereco = endereco;
-                this.Cep = cep;
-                this.Complemento = complemento;
-                this.Bairro = bairro;
-                this.Login = login;
-                this.Senha = senha;
-                this.Dieta = dieta;
-                this.Agconsulta = agconsulta;
-                this.Peso = peso;
-                this.Altura = altura;
-            }
-        }
+    }
 
 
     
