1ef3fd73964e1f34c2f08dafa049786302266eb0
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-24 22:25:43 -0300

    Criaçao da classe Endereco do paciente

---
+++ Apetit_API_2019/Apetit_API_2019/Models/Endereco_paciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apetit_API_2019.Models
+{
+    public class Endereco_paciente
+    {
+       int Id_endereco_paciente { get; set; }
+       int Id_paciente { get; set;}
+       String nome_rua { get; set;}
+       String num_rua { get; set;} 
+       String Cidade { get; set;}
+       String Bairro { get; set;}
+       
+    }
+}
