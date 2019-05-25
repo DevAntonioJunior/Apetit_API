a45c51c0fef6c0e3ec7601ff5f28f59c0a0bdd8e
Antonio Junior <Junior_08Imperador@msn.com>
2019-05-24 23:25:46 -0300

    criacao da classe Dieta

---
+++ Apetit_API_2019/Apetit_API_2019/Models/Dieta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apetit_API_2019.Models
+{
+    public class Dieta
+    {
+        int id_dieta { get; set;}
+        int id_paciente { get; set;}
+        int crn { get; set; }
+        String descricao { get;set;}
+    }
+}
